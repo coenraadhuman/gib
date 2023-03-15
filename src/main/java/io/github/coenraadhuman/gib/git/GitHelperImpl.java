79ebe6ea539155d3d675331210863064ba2ab49f
Coenraad Human <coen.human@gmail.com>
2023-03-15 21:32:26 +0200

    chore: change logging level

--- src/main/java/io/github/coenraadhuman/gib/git/GitHelperImpl.java
+++ src/main/java/io/github/coenraadhuman/gib/git/GitHelperImpl.java
@@ -62,11 +62,9 @@ public class GitHelperImpl implements GitHelper {
 
   @Override
   public Commit buildGitHookCommit(String gitHookMessage) {
-    log.debug(gitHookMessage);
     var dirtyCommit = DirtyCommit
                               .builder()
                               .message(gitHookMessage)
-                              // Todo: what does the footer look like?
                               .build();
     return buildCommit(dirtyCommit);
   }
