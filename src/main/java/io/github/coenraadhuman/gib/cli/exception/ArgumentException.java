88268cf3737c2ab95fddbcd89634ead034186744
Coenraad Human <coen.human@gmail.com>
2023-03-22 20:36:37 +0200

    refactor: remove dead code

--- src/main/java/io/github/coenraadhuman/gib/cli/exception/ArgumentException.java
+++
@@ -1,9 +0,0 @@
-package io.github.coenraadhuman.gib.cli.exception;
-
-public class ArgumentException extends RuntimeException {
-
-  public ArgumentException(String message) {
-    super(message);
-  }
-
-}
--- src/main/java/io/github/coenraadhuman/gib/command/common/Command.java
+++
@@ -1,7 +0,0 @@
-package io.github.coenraadhuman.gib.command.common;
-
-public interface Command<T> {
-
-  void execute(T argument);
-
-}
