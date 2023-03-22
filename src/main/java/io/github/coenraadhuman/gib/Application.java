c4202bd90388e147ddb6fd25c080c50a1dc4ef8a
Coenraad Human <coen.human@gmail.com>
2023-03-22 20:35:59 +0200

    style: checkstyle accommodation for long string lines

--- src/main/java/io/github/coenraadhuman/gib/Application.java
+++ src/main/java/io/github/coenraadhuman/gib/Application.java
@@ -1,8 +1,13 @@
 package io.github.coenraadhuman.gib;
 
+import io.github.coenraadhuman.clap.ClapApplication;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
+@ClapApplication(
+        description = "Gibberish git history analyser, a terminal utility that uses conventional commits to analyse your git history."
+)
 @SpringBootApplication
 public class Application {
 
