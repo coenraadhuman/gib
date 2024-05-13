a2c8ce55dcc0c13b2e5dd219ed7c720af3fb82c1
Coenraad Human <coen.human@gmail.com>
2024-05-13 09:13:13 +0200

    feat: regex for ensuring string is valid before processing

--- src/version.rs
+++ src/version.rs
@@ -1,3 +1,7 @@
+use regex::Regex;
+
+
+
 pub struct Version {
 
     pub major: u8,
@@ -19,23 +23,27 @@ impl Version {
 pub fn add_commit_to_version(major: u8, minor: u8, patch: u8, commit: &str) -> Version {
     let commit_string = commit.to_owned();
 
-    if commit_string.contains("!") || commit_string.contains("BREAKING_CHANGE") {
-        return Version::new(major + 1, 0, 0);
-    }
+    let conventional_commit_regex = Regex::new(r"^(build|chore|ci|docs|feat|fix|perf|refactor|revert|style|test)(\([a-z ]+\))?: [\w ]+").unwrap();
 
-    if commit_string.contains("feat") {
-        return Version::new(major, minor + 1, 0);
-    }
+    if conventional_commit_regex.is_match(commit) {
+        if commit_string.contains("!") || commit_string.contains("BREAKING_CHANGE") {
+            return Version::new(major + 1, 0, 0);
+        }
 
-    if commit_string.contains("refactor")
-        || commit_string.contains("perf")
-        || commit_string.contains("fix")
-        || commit_string.contains("chore")
-        || commit_string.contains("revert")
-        || commit_string.contains("docs")
-        || commit_string.contains("build")
-    {
-        return Version::new(major, minor , patch + 1);
+        if commit_string.contains("feat") {
+            return Version::new(major, minor + 1, 0);
+        }
+
+        if commit_string.contains("refactor")
+            || commit_string.contains("perf")
+            || commit_string.contains("fix")
+            || commit_string.contains("chore")
+            || commit_string.contains("revert")
+            || commit_string.contains("docs")
+            || commit_string.contains("build")
+        {
+            return Version::new(major, minor , patch + 1);
+        }
     }
 
     return Version { major: major, minor: minor, patch: patch };
