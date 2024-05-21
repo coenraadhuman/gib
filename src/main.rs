533e2e7d2344a5c1c444aa60614220775286467c
Coenraad Human <coen.human@gmail.com>
2024-05-21 14:20:29 +0200

    feat: initial changelog files

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 mod args;
 mod commands;
-mod version;
+mod semantic;
 mod tests;
 
 use args::Args;
@@ -12,6 +12,9 @@ fn main() {
     match args.command {
         args::Commands::Version { path, major, minor, patch, commit_git_hook, scope_filter } => {
             commands::version::run(path, major, minor, patch, commit_git_hook, scope_filter);
+        },
+        args::Commands::Changelog { path, commit_git_hook, scope_filter } => {
+            commands::changelog::run(path, commit_git_hook, scope_filter);
         },
     }
 
